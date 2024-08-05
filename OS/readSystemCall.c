b82c0a3ce60a59977d8e3cc36a3b916dc9c4c37c
kushal <kgoel8451@gmail.com>
2024-08-06 01:31:55 +0530

    os - worked with  open , write , append functionality

--- OS/readSystemCall.c
+++ OS/readSystemCall.c
@@ -4,20 +4,36 @@
 
 #define BUF_SIZE 1024
 
-int main(int argc , char* argv[])
+int main()
 {
-	int fd = open(argv[1],O_RDONLY);
-	char buf[BUF_SIZE];
-	if(fd != -1)
-        {
-		ssize_t numRead = read(fd , buf , BUF_SIZE-1);
-		while(numRead > 0)
-		{
-			buf[numRead] = '\0';
-			printf("%s \n" , buf);
-			numRead = read(fd , buf , BUF_SIZE-1);
-		}
+	int fd1 = open("sample.txt",O_RDONLY);
+	int fd2 = open("fd2.txt",O_WRONLY | O_TRUNC);
+	int fd3 = open("fd3.txt", O_WRONLY | O_APPEND | O_CREAT, 0644);
+	int fd4 = open("fd4.txt", O_WRONLY | O_APPEND);
+	if(fd4 == -1)
+	{
+	    printf("unable to open file  that does not exist \n");
 	}
+	if(fd2 == -1)
+	{
+	    printf("unable to open file fd2.txt \n");
+	    return -1;
+	}
+	char buf[BUF_SIZE];
+	if(fd1 != -1)
+        {
+		int numRead = read(fd1, buf , 1024);
+		write(STDOUT_FILENO, buf, numRead);
+		write(fd2 , buf , numRead);
+		write(fd3 , buf , numRead);
+		int flag = write(fd4 , buf , numRead);
+		if(flag == -1)
+		{
+		  printf("Nothing updated");
+		}
+                close(fd1);
+                close(fd2);
+      	}
 	else 
         {
 		printf("Error to  read file \n");
